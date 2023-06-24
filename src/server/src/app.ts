4b70c67a46b35176f1f1b4b9a4bedfc98f2e4c79
Kevin Lee <kevinlee3627@gmail.com>
2023-06-24 13:38:21 -0500

    listen

--- src/server/src/app.ts
+++ src/server/src/app.ts
@@ -1,7 +1,7 @@
 import express, { type Request, type Response } from 'express';
 import helmet from 'helmet';
 
-const app = express();
+export const app = express();
 
 app.use(helmet());
 app.use(express.json());
--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -1,3 +1,11 @@
-export function add(x: number, y: number): number {
-  return x + y;
+import { app } from './app';
+
+async function main(): Promise<void> {
+  app.listen(8443, () => {
+    console.log('Listening on 8443');
+  });
 }
+
+main().catch((err) => {
+  console.log(err);
+});
