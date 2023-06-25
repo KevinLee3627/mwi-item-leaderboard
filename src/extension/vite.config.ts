5ab1bd0e107a63f056664779fb319df43261199d
Kevin Lee <kevinlee3627@gmail.com>
2023-06-25 11:32:06 -0500

    Remove test.milkywayidle.com

--- src/extension/vite.config.ts
+++ src/extension/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
         'run-at': 'document-start',
         icon: 'https://vitejs.dev/logo.svg',
         namespace: 'mwi-item-leaderboards',
-        match: ['*://*www.milkywayidle.com/*', '*://*test.milkywayidle.com/*'],
+        match: ['*://*www.milkywayidle.com/*'],
       },
     }),
   ],
