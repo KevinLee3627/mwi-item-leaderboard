52c98e883f99efe5fdf6cb8829a141fd7be1d913
Kevin Lee <kevinlee3627@gmail.com>
2023-06-25 18:41:08 -0500

    Setup dotenv + https

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -1,10 +1,20 @@
+import { readFileSync } from 'fs';
+import https from 'https';
 import { app } from './app';
+
 async function main(): Promise<void> {
-  app.listen(8443, () => {
+  const privateKey = readFileSync(process.env.KEY_PATH as string, 'utf8');
+  const certificate = readFileSync(process.env.CERT_PATH as string, 'utf8');
+  const config = {
+    key: privateKey,
+    cert: certificate,
+  };
+
+  const server = https.createServer(config, app);
+  server.listen(8443, () => {
     console.log('Listening on 8443');
   });
 }
-
 main().catch((err) => {
   console.log(err);
 });
