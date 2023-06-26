c2cbdf86e85342bf06ab3b193791c752f82d1312
Kevin Lee <kevinlee3627@gmail.com>
2023-06-25 19:48:09 -0500

    light styling

--- src/client/src/components/Leaderboard.tsx
+++ src/client/src/components/Leaderboard.tsx
@@ -7,22 +7,22 @@ interface LeaderboardProps {
 export function Leaderboard(props: LeaderboardProps) {
   const rows = props.data.map((entry) => {
     return (
-      <tr key={entry.playerId}>
-        <td>{entry.player.displayName}</td>
-        <td>{entry.num}</td>
-        <td>{entry.itemEnhancementLevel}</td>
-        <td>{entry.ts}</td>
+      <tr key={entry.playerId} className='text-center'>
+        <td className='p-2'>{entry.player.displayName}</td>
+        <td className='p-2'>{entry.num}</td>
+        <td className='p-2'>{entry.itemEnhancementLevel}</td>
+        <td className='p-2'>{entry.ts}</td>
       </tr>
     );
   });
   return (
     <table className='table-auto'>
-      <thead>
-        <tr>
-          <td>Player</td>
-          <td>#</td>
-          <td>Enhancement Level</td>
-          <td>Last Updated</td>
+      <thead className='border-b-2 border-black'>
+        <tr className='text-left p-4'>
+          <td className='p-2'>Player</td>
+          <td className='p-2'>#</td>
+          <td className='p-2'>Enhancement Level</td>
+          <td className='p-2'>Last Updated</td>
         </tr>
       </thead>
       <tbody>{rows}</tbody>
