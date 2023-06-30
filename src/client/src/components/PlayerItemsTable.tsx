cdbf99b07dde8833591937b32ba0ab8d6613ecf2
Kevin Lee <kevinlee3627@gmail.com>
2023-06-29 23:07:01 -0500

    PlayerItemsTable uses table component

--- src/client/src/components/PlayerItemsTable.tsx
+++ src/client/src/components/PlayerItemsTable.tsx
@@ -1,39 +1,47 @@
 import { Link } from 'react-router-dom';
 import { GetItemLeaderboardReturn } from 'server';
 import { hridToDisplayName } from '../util/hridToDisplayName';
+import { Table } from './Table';
 
 interface PlayerItemsTableProps {
   data: GetItemLeaderboardReturn[];
 }
 
-export function PlayerItemsTable(props: PlayerItemsTableProps) {
-  const rows = props.data.map((entry, i) => {
-    return (
-      <tr key={i} className='hover text-left'>
-        <td className='p-2 underline'>
-          <Link
-            to={`/item/?itemHrid=${entry.itemHrid}&enhancementLevel=${entry.itemEnhancementLevel}&limit=100`}
-          >
-            {hridToDisplayName(entry.itemHrid)}
-          </Link>
-        </td>
-        <td className='p-2'>{entry.itemEnhancementLevel}</td>
-        <td className='p-2'>{entry.num.toLocaleString()}</td>
-        <td className='p-2'>{new Date(entry.ts).toLocaleString()}</td>
-      </tr>
-    );
-  });
+export function PlayerItemsTable({ data }: PlayerItemsTableProps) {
   return (
-    <table className='table table-zebra mx-auto w-6/12'>
-      <thead className='border-b-2 border-black'>
-        <tr className='text-left p-4'>
-          <td className='p-2'>Item</td>
-          <td className='p-2'>Enhancement Level</td>
-          <td className='p-2'>#</td>
-          <td className='p-2'>Last Updated</td>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </table>
+    <Table
+      data={data.map((entry) => ({
+        itemName: hridToDisplayName(entry.itemHrid),
+        itemHrid: entry.itemHrid,
+        enhancementLevel: entry.itemEnhancementLevel,
+        amount: entry.num,
+        lastUpdated: entry.ts,
+      }))}
+      headers={[
+        { key: 'itemName', label: 'Item' },
+        { key: 'enhancementLevel', label: 'Enhancement Level' },
+        { key: 'amount', label: '#' },
+        { key: 'lastUpdated', label: 'Last Updated' },
+      ]}
+      defaultColumn='itemName'
+      row={(entry, i) => {
+        return (
+          <tr key={i} className='hover text-left'>
+            <td className='p-2 underline'>
+              <Link
+                to={`/item/?itemHrid=${entry.itemHrid}&enhancementLevel=${entry.enhancementLevel}&limit=100`}
+              >
+                {hridToDisplayName(entry.itemHrid)}
+              </Link>
+            </td>
+            <td className='p-2'>{entry.enhancementLevel}</td>
+            <td className='p-2'>{entry.amount.toLocaleString()}</td>
+            <td className='p-2'>
+              {new Date(entry.lastUpdated).toLocaleString()}
+            </td>
+          </tr>
+        );
+      }}
+    />
   );
 }
