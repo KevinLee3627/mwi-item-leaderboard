77379c0b0d23aed2a0fe0c12b8010165a1f2d729
Kevin Lee <kevinlee3627@gmail.com>
2023-07-01 17:24:05 -0500

    Update import path

--- src/server/src/ability/controller.ts
+++ src/server/src/ability/controller.ts
@@ -1,4 +1,4 @@
-import { getAllAbilityMetadata as getAllAbilityMetadataService } from 'src/services/getAllAbilityMetadata';
+import { getAllAbilityMetadata as getAllAbilityMetadataService } from 'src/ability/services/getAllAbilityMetadata';
 import { asyncHandler } from 'src/asyncHandler';
 
 const getAllAbilityMetadata = asyncHandler(async (req, res, next) => {
