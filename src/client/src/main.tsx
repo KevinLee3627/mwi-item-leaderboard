3232826787b14ed30715c36910cf49d65ad846e9
Kevin Lee <kevinlee3627@gmail.com>
2023-07-01 18:31:40 -0500

    Set to index

--- src/client/src/main.tsx
+++ src/client/src/main.tsx
@@ -87,7 +87,7 @@ const router = createBrowserRouter([
         },
       },
       {
-        path: '/',
+        index: true,
         element: <ItemLeaderboard />,
         loader: async () => {
           const { data: itemMetadata } = await axios.get<GetAllItemMetadataRes>(
