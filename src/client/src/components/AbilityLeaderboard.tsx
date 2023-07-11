297f7ce7384d6e4d92e027af8adafee26fae9981
Kevin Lee <kevinlee3627@gmail.com>
2023-07-10 22:13:37 -0500

    Adjust AbilityLeaderboard

--- src/client/src/components/AbilityLeaderboard.tsx
+++ src/client/src/components/AbilityLeaderboard.tsx
@@ -1,7 +1,9 @@
+import { useRef } from 'react';
+import { useLocation, useNavigate } from 'react-router';
 import { Link, useLoaderData } from 'react-router-dom';
+import Select from 'react-select';
 import { GetAbilityLeaderboardRes, GetAllAbilityMetadataRes } from 'server';
 import { hridToDisplayName } from 'util/hridToDisplayName';
-import { AbilitySearchBox } from 'components/AbilitySearchBox';
 import { Table } from 'components/Table';
 import { getRankIcon } from 'util/getRankIcon';
 
@@ -71,5 +73,55 @@ export function AbilityLeaderboard() {
         />
       </div>
     </>
+  );
+}
+
+export interface AbilitySearchOption<T> {
+  label: string;
+  value: T;
+}
+
+interface SearchBoxProps {
+  options?: AbilitySearchOption<GetAllAbilityMetadataRes[0]>[];
+  loading: boolean;
+}
+
+export function AbilitySearchBox(props: SearchBoxProps) {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const searchParams = useRef(new URLSearchParams(location.search.slice(1)));
+
+  return (
+    <div className='md:w-6/12 mx-auto flex-1 '>
+      <Select
+        isSearchable
+        options={props?.options}
+        defaultValue={{
+          label: hridToDisplayName(
+            hridToDisplayName(searchParams.current.get('abilityHrid') ?? '')
+          ),
+          value: {
+            hrid: searchParams.current.get('abilityHrid'),
+            displayName: hridToDisplayName(
+              searchParams.current.get('abilityHrid') ?? ''
+            ),
+          },
+        }}
+        placeholder={props.loading ? 'Loading options...' : 'Search items'}
+        onChange={(newValue) => {
+          const searchParams = new URLSearchParams(location.search.slice(1));
+          searchParams.set('abilityHrid', String(newValue?.value.hrid));
+          navigate(`/leaderboard/ability?${searchParams.toString()}&limit=100`);
+        }}
+        styles={{
+          option: (base) => {
+            return {
+              ...base,
+              color: 'black',
+            };
+          },
+        }}
+      />
+    </div>
   );
 }
