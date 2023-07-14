a2c2da8371bfac5ae9e52ac458ba6d10998c325d
Kevin Lee <kevinlee3627@gmail.com>
2023-07-14 19:20:48 +0000

    Remove https from express b/c of nginx

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -1,16 +1,8 @@
-import { readFileSync } from 'fs';
-import https from 'https';
+import http from 'http';
 import { app } from './app';
 
 async function main(): Promise<void> {
-  const privateKey = readFileSync(process.env.KEY_PATH as string, 'utf8');
-  const certificate = readFileSync(process.env.CERT_PATH as string, 'utf8');
-  const config = {
-    key: privateKey,
-    cert: certificate,
-  };
-
-  const server = https.createServer(config, app);
+  const server = http.createServer(app);
   server.listen(8443, () => {
     console.log('Listening on 8443');
   });
