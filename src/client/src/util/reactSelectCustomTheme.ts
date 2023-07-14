12782b34f78cb9b20716789f64d8e197b27edfbc
Kevin Lee <kevinlee3627@gmail.com>
2023-07-14 00:04:51 -0500

    Update theme

--- src/client/src/util/reactSelectCustomTheme.ts
+++ src/client/src/util/reactSelectCustomTheme.ts
@@ -4,6 +4,7 @@ export const customColors: Partial<Theme['colors']> = {
   primary: 'hsl(var(--p))', //
   primary25: 'hsl(var(--nf))', // option hover
   neutral0: 'hsl(var(--b1))', // input bg
+  neutral10: 'hsl(var(--p))', // multi option bg
   neutral20: 'hsl(var(--p))', // input border, divider, chevron
   neutral30: 'hsl(var(--p))', // input border hover
   neutral80: 'hsl(var(--nc))', // select text
