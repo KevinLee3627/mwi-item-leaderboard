118941378984c7ca9423b8d26abfcb1e96f77211
Kevin Lee <kevinlee3627@gmail.com>
2023-08-10 19:56:54 -0500

    fix: Put status router in main router...

--- src/server/src/router.ts
+++ src/server/src/router.ts
@@ -4,6 +4,7 @@ import { router as leaderboardRouter } from 'src/leaderboard/router';
 import { router as itemRouter } from 'src/item/router';
 import { router as abilityRouter } from 'src/ability/router';
 import { router as gameRouter } from 'src/game/router';
+import { router as serviceStatusRouter } from 'src/status/router';
 
 export const router = Router();
 
@@ -12,3 +13,4 @@ router.use(leaderboardRouter);
 router.use(itemRouter);
 router.use(abilityRouter);
 router.use(gameRouter);
+router.use(serviceStatusRouter);
