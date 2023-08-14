3f58eb65b65907c033158a560660161736022747
Kevin Lee <kevinlee3627@gmail.com>
2023-08-14 09:04:35 -0500

    Update status endpoint

--- src/server/src/status/services/getScraperStatus.ts
+++ src/server/src/status/services/getScraperStatus.ts
@@ -5,9 +5,20 @@ export async function getScraperStatus(): Promise<ScraperStatus> {
   const res = await prisma.serviceStatus.findUnique({
     where: { name: 'scraper' },
   });
+
   if (res == null) {
     throw new Error('Could not find scraper status');
   }
 
-  return res.status as ScraperStatus;
+  const ONE_MINUTE = 1000 * 60;
+  const dateDifference = new Date().getTime() - res.lastUpdate.getTime();
+  if (Math.abs(dateDifference) > ONE_MINUTE) {
+    await prisma.serviceStatus.update({
+      where: { name: 'scraper' },
+      data: { status: 'down', lastUpdate: new Date() },
+    });
+    return 'down';
+  } else {
+    return res.status as ScraperStatus;
+  }
 }
